4ea930a22e63c0cfbb29d536f4c13e0afbad90b7
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-02-26 14:46:53 +0000

    Listas de exercicios

---
+++ lista1.js
@@ -0,0 +1,45 @@
+//Criar um programa para exibir a mensagem: "Olá, Mundo!"
+let mensagem = "Olá, Mundo!";
+console.log(mensagem);
+//Criar um programa para exibir seu nome completo
+let nomeCompleto = "Igor Araujo do Nascimento";
+console.log(nomeCompleto);
+//Criar um programa que mostre um numero e exiba seu dobro
+let numero = 5;
+let dobro = numero * 2;
+console.log("O dobro de " + numero + " é: " + dobro);
+//Criar um programa que mostre um numwro e exiba o triplo
+let triplo = numero * 3;
+console.log("O triplo de " + numero + " é: " + triplo);
+//Criar um programa que msotre duas notas e calcule a media
+let nota1 = 8;
+let nota2 = 7;
+let media = (nota1 + nota2) / 2;
+console.log("A média das notas é: " + media);
+//Criar umporgrama para conveter metros em centimetros
+let metros = 2.5;
+let centimetros = metros * 100;
+console.log(metros + " metros é igual a " + centimetros + " centímetros");
+//Criar um programa para mostrar o antecessor e sucessor de um numero
+let numAntecessor = numero - 1;
+let numSucessor = numero + 1;
+console.log("O antecessor de " + numero + " é: " + numAntecessor);
+console.log("O sucessor de " + numero + " é: " + numSucessor);
+//Criar um programa que mostre a area de um triângulo
+let base = 10;
+let altura = 5;
+let areaTriangulo = (base * altura) / 2;
+console.log("A área do triângulo é: " + areaTriangulo);
+//Criar um programa para calcular um aumento de 10% em um salario de 2000,00.
+let salario = 2000.00;
+let aumento = salario * 0.10;
+let novoSalario = salario + aumento;
+console.log("O novo salário com aumento de 10% é: " + novoSalario.toFixed(2));
+//Criar um programa para verificar se um numero é par ou impar
+let numeroVerificar = 7;
+if (numeroVerificar % 2 === 0) {
+    console.log(numeroVerificar + " é um número par.");
+} else {
+    console.log(numeroVerificar + " é um número ímpar.");
+}
+
