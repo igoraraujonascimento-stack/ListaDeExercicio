a685031daa18236fa65c3323d19d16a3361f225d
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-02-27 13:11:30 +0000

    Lista de exercicio 2

--- lista2.js
+++ lista2.js
@@ -1,19 +1,29 @@
 //Criar um programa quese um numero é positivo ou negativo
+let numero = -5;
 
-const numero = -5;
 if (numero > 0) {
     console.log("O número é positivo.");
-}else if (numero < 0) {
+} else if (numero < 0) {
     console.log("O número é negativo.");
-}else {
+} else {
     console.log("O número é zero.");
 }
-
 //Criar um programa se um ano é bissexto
+let ano = 2024;
 
-let num1 = 2010
-if (ano % 4 === 0)
-    console.log ('bissexto') 
+if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
+    console.log(ano + " é um ano bissexto.");
 } else {
-    console.log ('não é bissexto')
+    console.log(ano + " não é um ano bissexto.");
 }
+//Criar um programa que mostre uma senha e a mensagem acesso permitido
+let senha = "sixseven67";
+if (senha === "sixseven67") {
+    console.log("Acesso permitido.");
+} else {    console.log("Acesso negado.");
+}
+//Criar um programa que mostre um desconto de 5% para compras maiores que R$100
+//Criar um programa que classifique uma pessoa como criança, adolescente, ou adulto de acordo com a idade
+//Criar um programa que mostre um triamgulo de acordo com seus lados: (Equilátero, Isósceles ou Escaleno)
+//Criar um programa que mostre conceitos  A B C ou  D e de acordo com as notas: >9.5 A; Entre 9.4 e 8.5 B; Entre 8.4 e 7.0 C; <6.9 D
+
