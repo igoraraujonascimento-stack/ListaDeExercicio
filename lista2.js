16fbf0df2e12a3a0922912fcea484e6eb472e0c2
Igor Araújo do Nascimento <igor.araujo.nascimento@escola.pr.gov.br>
2026-02-27 09:23:20 -0300

    lista de exercicios 2

---
+++ lista2.js
@@ -0,0 +1,19 @@
+//Criar um programa quese um numero é positivo ou negativo
+
+const numero = -5;
+if (numero > 0) {
+    console.log("O número é positivo.");
+}else if (numero < 0) {
+    console.log("O número é negativo.");
+}else {
+    console.log("O número é zero.");
+}
+
+//Criar um programa se um ano é bissexto
+
+let num1 = 2010
+if (ano % 4 === 0)
+    console.log ('bissexto') 
+} else {
+    console.log ('não é bissexto')
+}
